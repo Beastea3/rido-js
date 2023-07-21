1d469214386917440b38e8065df810ca3f232394
Monstea <yzhao9481@gmail.com>
2023-07-21 17:42:32 +0800

    added auth helper

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import RidoClient from "./client";
-import VONFT_Client from "./clients/vonftClient";
-import VOSBT_Client from "./clients/vosbtClient";
+import VONFT_Client from "./client/vonftClient";
+import VOSBT_Client from "./client/vosbtClient";
+import authClient from "./helper/identity";
 
 export default RidoClient;
-export { VONFT_Client, VOSBT_Client };
+export { VONFT_Client, VOSBT_Client, authClient };
